26f71dbdd2ca1b1769b0405a6496218ef112e2dc
John Murray <5672686+JRMurr@users.noreply.github.com>
2020-12-11 21:58:16 -0500

    day 11

--- day11/Main.hs
+++ day11/Main.hs
@@ -1,16 +1,11 @@
 module Main where
-
-import Data.Maybe
-import Data.List.Split
-
+import Data.Maybe ( catMaybes, fromJust )
+import Data.List ( intercalate )
+import Data.Map (Map)
+import qualified Data.Map as Map
 data Cell = Floor | Occupied | Empty deriving (Show, Eq)
-
-data Grid = Grid {cells :: [[Cell]], width :: Int, height :: Int} deriving (Show)
-
 type Cord = (Int, Int) -- (x,y)
-
-tuplify2 :: [a] -> (a, a)
-tuplify2 [x, y] = (x, y)
+type Grid = Map Cord Cell
 
 charToCell :: Char -> Cell
 charToCell x = case x of
@@ -22,52 +17,100 @@ charToCell x = case x of
 parseLine :: String -> [Cell]
 parseLine = map charToCell
 
+mapWithIdx :: ((Int, a) -> b) -> [a] -> [b]
+mapWithIdx f = zipWith (curry f) [0 .. ]
+
+
+addCords :: [[Cell]] -> Grid
+addCords lst = 
+  Map.fromList (concat $ mapWithIdx (\ (y, row) -> mapWithIdx (\ (x, cell) -> ((x, y), cell)) row) lst)
+
+
 getGrid :: [String] -> Grid
-getGrid input =
-  let cells = map parseLine input
-   in Grid {cells = cells, width = length $ head cells, height = length cells}
+getGrid lst = addCords (map parseLine lst)
 
 getCell :: Grid -> Cord -> Maybe Cell
-getCell grid (x, y)
-  | width grid <= x || x < 0 = Nothing
-  | height grid <= y || y < 0 = Nothing
-  | otherwise = Just ((cells grid !! y) !! x)
+getCell grid cord = Map.lookup cord grid
 
--- zip [0..(width grid -1)] [0..(height grid - 1)]
+getAdjacentCells:: Grid -> Cord -> [Cell]
+getAdjacentCells grid (x0, y0) = 
+  catMaybes [getCell grid (x, y) | x <- [x0 -1 .. x0 + 1], y <- [y0 -1 .. y0 + 1], not (x0 == x && y0 == y)]
 
-runRuleOnCell :: Grid -> Cord -> Cell
-runRuleOnCell grid (x0, y0) = case currentCell of
-  Nothing -> error "cell does not exist"
-  Just Floor -> Floor
-  Just Occupied -> if numOccupied >= 4 then Empty else Occupied
-  Just Empty -> if numOccupied == 0 then Occupied else Empty
+runRuleOnCellP1 :: Grid -> Cord -> Cell -> Cell
+runRuleOnCellP1 grid currCord currentCell =
+  case currentCell of
+  Floor -> Floor
+  Occupied -> if numOccupied >= 4 then Empty else Occupied
+  Empty -> if numOccupied == 0 then Occupied else Empty
   where
-    adjacentCells = catMaybes [getCell grid (x, y) | x <- [x0 -1 .. x0 + 1], y <- [y0 -1 .. y0 + 1], not (x0 == x && y0 == y)]
-    numOccupied = length (filter (== Occupied) adjacentCells)
-    currentCell = getCell grid (x0, y0)
+    numOccupied = length (filter (== Occupied) (getAdjacentCells grid currCord))
 
-runRules:: Grid -> Grid
-runRules grid =
-  let allCords = [(x,y) | x <- [0..(width grid -1)], y <- [0..(height grid -1)]] in
-    let newCells = chunksOf (width grid) (map ( runRuleOnCell grid) allCords) in
-      if newCells == cells grid
-        then grid
-        else runRules grid {cells=newCells}
+runRules:: Grid -> (Grid -> Cord -> Cell -> Cell) -> Grid
+runRules grid cellRule =
+  let newGrid = Map.mapWithKey (cellRule grid) grid in
+    if newGrid == grid 
+      then grid
+      else runRules newGrid cellRule
 
 part1 :: IO ()
 part1 = do
   lines <- getInput
   let grid = getGrid lines
-  let finalGrid = runRules grid
-  print finalGrid
+  let finalGrid = runRules grid runRuleOnCellP1
+  print $ length (Map.filter (==Occupied) finalGrid)
+
+addSlope :: Cord -> (Int,Int) -> Cord
+addSlope cord slope =
+  (fst cord + fst slope, snd cord + snd slope)
+
+followSlope:: Grid -> Cord -> (Int,Int) -> Maybe Cell
+followSlope grid cord slope = 
+  case getCell grid cord of
+    Just Occupied -> Just Occupied
+    Just Empty -> Just Empty
+    Just Floor -> followSlope grid (addSlope cord slope) slope
+    Nothing -> Nothing
+
+getSeenCells :: Grid -> Cord -> [Cell]
+getSeenCells grid currCord =
+  catMaybes [followSlope grid (addSlope currCord (x,y)) (x, y) | x<-[-1..1], y<-[-1..1], not (x==0 && y==0)]
+
+
+runRuleOnCellP2 :: Grid -> Cord -> Cell -> Cell
+runRuleOnCellP2 grid currCord currentCell =
+  case currentCell of
+    Floor -> Floor
+    Occupied -> if length occupiedCells >= 5 then Empty else Occupied
+    Empty -> if null occupiedCells then Occupied else Empty
+    where
+      occupiedCells = filter (== Occupied) (getSeenCells grid currCord)
 
 part2 :: IO ()
 part2 = do
   lines <- getInput
-  print lines
+  let grid = getGrid lines
+  let finalGrid = runRules grid runRuleOnCellP2
+  print $ length (Map.filter (==Occupied) finalGrid)
 
 getInput :: IO [String]
-getInput = lines <$> readFile "./in.example"
+getInput = lines <$> readFile "./in"
+
+
+cellToChar :: Cell -> Char
+cellToChar cell = 
+  case cell of
+    Empty -> 'L'
+    Occupied -> '#'
+    Floor -> '.'
+
+showGrid :: Grid -> [Char]
+showGrid m =
+  let (x1,y1) = maximum $ Map.keys m in
+  let (x0,y0) = minimum $ Map.keys m in
+  let getRow y = [cellToChar $ fromJust (Map.lookup (x,y) m) | x<-[x0..x1]] in
+  let lines = [getRow y | y<- [y0..y1]] in
+  intercalate "\n" lines
+
 
 main :: IO ()
-main = part1
+main = part2
