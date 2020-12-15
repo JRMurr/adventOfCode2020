54031ad4817328dfbaa7053c57944cfa910e70e9
John Murray <5672686+JRMurr@users.noreply.github.com>
2020-12-14 19:56:29 -0500

    day 14 part 2

--- day14/Main.hs
+++ day14/Main.hs
@@ -7,7 +7,7 @@ import qualified Data.Map as Map
 
 type BinaryNum = [Int]
 
-type MemAssignment = (Int, BinaryNum) -- (addr, value)
+type MemAssignment = (Int, Int) -- (addr, value)
 
 type MaskAssignments = (String, [MemAssignment])
 
@@ -32,41 +32,65 @@ fromBinary lst =
   let lstWithIdx = zip [0 ..] (reverse lst)
    in sum [2 ^ idx | (idx, x) <- lstWithIdx, x == 1]
 
-getMaskedDigit :: Char -> Int -> Int
-getMaskedDigit '1' _ = 1
-getMaskedDigit '0' _ = 0
-getMaskedDigit _ digit = digit
+getMaskedDigitP1 :: Char -> Int -> Int
+getMaskedDigitP1 '1' _ = 1
+getMaskedDigitP1 '0' _ = 0
+getMaskedDigitP1 _ digit = digit
 
-getMaskedNum :: String -> BinaryNum -> BinaryNum
-getMaskedNum mask num =
+getMaskedNumP1 :: String -> BinaryNum -> BinaryNum
+getMaskedNumP1 mask num =
   zipWith
-    (curry (\(char, digit) -> getMaskedDigit char digit))
+    (curry (\(char, digit) -> getMaskedDigitP1 char digit))
     mask
     num
 
-writeValue :: Memory -> String -> MemAssignment -> Memory
-writeValue memory mask (addr, value) =
-  let maskedVal = fromBinary (getMaskedNum mask value)
+writeValueP1 :: Memory -> String -> MemAssignment -> Memory
+writeValueP1 memory mask (addr, value) =
+  let maskedVal = fromBinary (getMaskedNumP1 mask (toBinary value))
    in Map.insert addr maskedVal memory
 
-writeMaskAssignments :: Memory -> MaskAssignments -> Memory
-writeMaskAssignments memory (mask, assignments) =
-  foldl (`writeValue` mask) memory assignments
+writeMaskAssignmentsP1 :: Memory -> MaskAssignments -> Memory
+writeMaskAssignmentsP1 memory (mask, assignments) =
+  foldl (`writeValueP1` mask) memory assignments
 
-writeAllMaskAssignments :: Memory -> [MaskAssignments] -> Memory
-writeAllMaskAssignments = foldl writeMaskAssignments
+writeAllMaskAssignmentsP1 :: Memory -> [MaskAssignments] -> Memory
+writeAllMaskAssignmentsP1 = foldl writeMaskAssignmentsP1
+
+addToFrontOfAll :: Int -> [BinaryNum] -> [BinaryNum]
+addToFrontOfAll newHead = map (newHead :)
+
+getMaskedAddrs :: String -> BinaryNum -> [BinaryNum]
+getMaskedAddrs mask binNum = case uncons mask of
+  Nothing -> [binNum]
+  Just ('0', t) -> addToFrontOfAll (head binNum) (getMaskedAddrs t (tail binNum))
+  Just ('1', t) -> addToFrontOfAll 1 (getMaskedAddrs t (tail binNum))
+  Just (_, t) ->
+    let res = getMaskedAddrs t (tail binNum)
+     in addToFrontOfAll 1 res ++ addToFrontOfAll 0 res
+
+writeValueP2 :: Memory -> String -> MemAssignment -> Memory
+writeValueP2 memory mask (addr, value) =
+  let maskedAddrs = getMaskedAddrs mask (toBinary addr)
+   in foldl (\acc v -> Map.insert (fromBinary v) value acc) memory maskedAddrs
+
+writeMaskAssignmentsP2 :: Memory -> MaskAssignments -> Memory
+writeMaskAssignmentsP2 memory (mask, assignments) =
+  foldl (`writeValueP2` mask) memory assignments
+
+writeAllMaskAssignmentsP2 :: Memory -> [MaskAssignments] -> Memory
+writeAllMaskAssignmentsP2 = foldl writeMaskAssignmentsP2
 
 part1 :: IO ()
 part1 = do
   input <- getInput
-  let memory = writeAllMaskAssignments Map.empty input
-  print memory
+  let memory = writeAllMaskAssignmentsP1 Map.empty input
   print $ sum (Map.elems memory)
 
 part2 :: IO ()
 part2 = do
   input <- getInput
-  print input
+  let memory = writeAllMaskAssignmentsP2 Map.empty input
+  print $ sum (Map.elems memory)
 
 replace old new = intercalate new . splitOn old
 
@@ -74,7 +98,7 @@ parseMemoryAssignment :: String -> MemAssignment
 parseMemoryAssignment str =
   let [keyStr, value] = splitOn " = " str
    in let key = replace "]" "" (replace "mem[" "" keyStr)
-       in (read key, toBinary $ read value)
+       in (read key, read value)
 
 parseMask :: String -> String
 parseMask = replace "mask = " ""
@@ -86,4 +110,4 @@ getInput = do
   return (map (\x -> (parseMask $ head x, map parseMemoryAssignment (tail x))) chunked)
 
 main :: IO ()
-main = part1
+main = part2
